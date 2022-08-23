4be466f6c960355ddb62cc11494635d4f7d59b2a
Ana Gomes <ana.siqueiragomes@gmail.com>
2022-08-23 14:44:28 -0500

    Countdown to is now countdown from

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // The date we're counting down to with Brasilia timezone
-var countDownDate = new Date("Fri Jul 29 2022 00:00:00 UTC-0300 (BRT)").getTime();
+var countDownDate = new Date().getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
 
 // Get today's date and time
-var now = new Date().getTime();
+var now = new Date("Fri Jul 02 2022 00:00:00 UTC-0300 (BRT)").getTime();
     
 // Find the distance between now and the count down date
 var distance = countDownDate - now;
@@ -21,11 +21,10 @@ document.getElementById("demo").innerHTML = days + "d " + hours + "h "
 + minutes + "m " + seconds + "s ";
     
 // If the count down is over, write some text 
+
 if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "Hello, Handsome!";
-}
-}, 1000);
+clearInterval(x);
+document.getElementById("demo").innerHTML = "See you soon"; } }, 1000);
 
 // Video Script
 
